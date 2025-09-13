c7a14ac4e0d7562541f5dc3fd502875b12ccd245
textbadal <textbadal@gmail.com>
2025-09-14 01:34:55 +0530

    19se

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
 import Hero from '@/components/Hero'
-import Services from "@/components/Services";
+
 import Gallery from "@/components/Gallery";
 import Testimonials from "@/components/Testimonials";
 import Contact from "@/components/Contact";
+import ProductsPage from './products/page';
+import ServicesPage from './services/page';
 
 
 export default function Page() {
   return (
     <>
       <Hero />
-      <Services />
+      <ServicesPage />
       <Gallery />
       <Testimonials />
+      <ProductsPage />
+      
       <Contact />
     </>
   )
