6b2d1e400cec251a477e2798019e91c428dea03e
textbadal <textbadal@gmail.com>
2026-04-21 02:57:22 +0530

    0730

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+
   images: {
     unoptimized: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
